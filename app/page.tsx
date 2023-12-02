a4f0ed1153953e7cdb8efefaaa18d5440d3bbf43
Ajay Pradhan <pradhanajay14@gmail.com>
2023-12-02 16:29:41 +0530

    hero done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Hero from '@/components/Hero'
+import Camp from "@/components/Camp";
+
 import Image from 'next/image'
+import Guide from '@/components/Guide';
+import Features from '@/components/Features';
+import GetApp from '@/components/GetApp';
 
 export default function Home() {
   return (
-    <h1 className=' text-teal-400 font-bold'>Hello world</h1>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp/>
+    </>
   )
 }
